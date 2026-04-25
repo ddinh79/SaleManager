6a2b10314bde798e0dced10c8920fceaa778c696
ddinh79 <ddinh79@gmail.com>
2026-04-25 16:51:15 +0700

    Add /users and /users/:id routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { Login } from './pages/Login';
 import { Dashboard } from './pages/Dashboard';
 import { Doctors } from './pages/Doctors';
 import { Hospitals } from './pages/Hospitals';
+import { Users } from './pages/Users';
+import { UserDetail } from './pages/UserDetail';
 import { NotFound } from './pages/NotFound';
 import { useAuthStore } from './store/authStore';
 
@@ -33,6 +35,8 @@ function App() {
           <Route index element={<Dashboard />} />
           <Route path="doctors" element={<Doctors />} />
           <Route path="hospitals" element={<Hospitals />} />
+          <Route path="users" element={<Users />} />
+          <Route path="users/:id" element={<UserDetail />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
