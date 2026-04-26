45e0fcd543e0fa24a28693eca978dc08e6419f44
ddinh79 <ddinh79@gmail.com>
2026-04-26 10:39:53 +0700

    feat(deals): add IDealService interface

---
+++ backend/Services/IDealService.cs
@@ -0,0 +1,15 @@
+using SalesSystem.DTOs;
+using SalesSystem.Entities;
+
+namespace SalesSystem.Services;
+
+public interface IDealService
+{
+    Task<DealResponse> CreateDealAsync(CreateDealRequest request, Guid salesId);
+    Task<DealResponse?> GetDealByIdAsync(Guid id);
+    Task<DealResponse?> UpdateDealAsync(Guid id, UpdateDealRequest request, Guid salesId, string userRole);
+    Task<bool> DeleteDealAsync(Guid id, Guid salesId, string userRole);
+    Task<PipelineResponse> GetPipelineAsync(Guid? managerId, string userRole, Guid currentUserId);
+    Task<ForecastResponse> GetForecastAsync();
+    Task<DealResponse?> UpdateStageAsync(Guid id, UpdateStageRequest request, Guid salesId, string userRole);
+}
